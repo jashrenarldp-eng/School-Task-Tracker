e5edd3f2e54c7e6061c253ef1cba624d90005789
jashren arl dp <jashrenarldp@gmail.com>
2026-09-08 14:36:16 +0800

    Update comments for clarity in sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'takeit-doit-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+// Install Event: Saves core web app files to browser cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event: Deletes older caches when you release updates
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event: Serves cached files if offline, otherwise fetches from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+}); 
